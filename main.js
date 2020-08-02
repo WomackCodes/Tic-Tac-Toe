6d3150a70d336ca665a9ad2ed8a4707dbe2d6901
WomackCodes <chris@womack.codes>
2020-08-02 14:59:38 -0700

    Transferred from other git repo

---
+++ main.js
@@ -0,0 +1,41 @@
+/*----- constants -----*/
+
+// 1.) Grid itself of 3x3
+// 2.) 
+// 3.) 
+
+
+
+/*----- app's state (variables) -----*/
+
+// 1.) player choices (AI/player 2 as well)
+// 2.) Wins vs Losses (for best of 3 maybe?)
+// 3.) Version of background maybe if you wanted theme
+// 4.) 
+
+
+
+/*----- cached element references -----*/
+
+// 1.) 
+// 2.) 
+// 3.) 
+
+
+
+/*----- event listeners -----*/
+
+
+// 1.) listen for click to the cell
+// 2.) hover pseudo-class listen to show symbol for used cells
+// 3.) event listener on button for new game
+// 3.) event listener on button for play again
+
+
+
+
+/*----- functions -----*/
+
+// 1.) 
+// 2.) 
+// 3.) 
